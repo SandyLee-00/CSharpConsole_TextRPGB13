fbd9c978abf9edaba806217a199fa45f0247fd35
Sandy <syjy8809@gmail.com>
2024-04-30 16:59:19 +0900

    feat : 프로젝트 시작

--- CSharpConsole_TextRPGB13/Program.cs
+++ CSharpConsole_TextRPGB13/Program.cs
@@ -1,10 +1,95 @@
-﻿namespace CSharpConsole_TextRPGB13
+﻿using System.Numerics;
+
+namespace TextRPG
 {
-    internal class Program
+    public class GameManager
     {
-        static void Main(string[] args)
+        private Player player;
+
+
+        public GameManager()
         {
-            Console.WriteLine("Hello, World!");
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            player = new Player("Jiwon", "Programmer", 1, 10, 5, 100, 15000);
+        }
+
+        public void StartGame()
+        {
+            Console.Clear();
+            MainMenu();
+        }
+
+        private void MainMenu()
+        {
+            // 구성
+            // 0. 화면 정리
+            Console.Clear();
+
+            // 1. 선택 멘트를 줌
+            Console.WriteLine("■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
+            Console.WriteLine("■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+            Console.WriteLine("");
+
+            Console.WriteLine("1. 상태보기");
+            Console.WriteLine("");
+
+            // 2. 선택한 결과를 검증함
+            int choice = Utility.PromptMenuChoice(1, 1);
+
+            // 3. 선택한 결과에 따라 보내줌
+            switch (choice)
+            {
+                case 1:
+                    StatusMenu();
+                    break;
+                
+            }
+            MainMenu();
+        }
+
+        private void StatusMenu()
+        {
+            Console.Clear();
+
+            Utility.ShowTitle("■ 상태보기 ■");
+            Console.WriteLine("캐릭터의 정보가 표기됩니다.");
+
+            Utility.PrintTextHighlights("Lv. ", player.Level.ToString("00"));
+            Console.WriteLine("");
+            Console.WriteLine($"{player.Name} ( {player.Job} )");
+
+            Utility.PrintTextHighlights("공격력 : ", (player.Atk).ToString());
+            Utility.PrintTextHighlights("방어력 : ", (player.Def).ToString());
+            Utility.PrintTextHighlights("체 력 : ", (player.Hp).ToString());
+
+            Utility.PrintTextHighlights("Gold : ", player.Gold.ToString());
+            Console.WriteLine("");
+
+            Console.WriteLine("0. 뒤로가기");
+            Console.WriteLine("");
+
+            switch (Utility.PromptMenuChoice(0, 0))
+            {
+                case 0:
+                    MainMenu();
+                    break;
+            }
+        }
+
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            GameManager gameManager = new GameManager();
+            gameManager.StartGame();
         }
     }
 }
---
+++ CSharpConsole_TextRPGB13/Utility.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TextRPG
+{
+    public class Utility
+    {
+        public static int PromptMenuChoice(int min, int max)
+        {
+            while (true)
+            {
+                Console.Write("원하시는 번호를 입력해주세요: ");
+                if (int.TryParse(Console.ReadLine(), out int choice) && choice >= min && choice <= max)
+                {
+                    return choice;
+                }
+                Console.WriteLine("잘못된 입력입니다. 다시 시도해주세요.");
+            }
+        }
+
+        internal static void ShowTitle(string title)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(title);
+            Console.ResetColor();
+        }
+
+        public static void PrintTextHighlights(string s1, string s2, string s3 = "")
+        {
+            Console.Write(s1);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(s2);
+            Console.ResetColor();
+            Console.WriteLine(s3);
+        }
+    }
+}
