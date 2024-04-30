b56fbc3438aa14ba048d5555076a9d00d0befb18
Sandy <syjy8809@gmail.com>
2024-04-30 16:17:13 +0900

    Add project files.

---
+++ CSharpConsole_TextRPGB13/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpConsole_TextRPGB13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
