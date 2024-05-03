43ab81f98d788659700c45ff8eef58f48de403d6
nakwony <nvvyun@gmail.com>
2024-05-03 13:56:29 +0900

    Feat: 출력 관련 커스텀 클래스, 알림 메시지 관련 클래스 추가

---
+++ CSharpConsole_TextRPGB13/Util/AlertMessage.cs
@@ -0,0 +1,12 @@
+namespace CSharpConsole_TextRPGB13.Util;
+
+public class AlertMessage
+{
+    public static AlertMessage Instance { get; } = new();
+
+    public const string COMPLETE_PURCHASE = "\n구매를 완료했습니다.";
+    public const string LACKED_GOLD = "\nGold 가 부족합니다.";
+    public const string PURCHASED = "\n이미 구매한 아이템입니다.";
+    public const string INPUT_ERROR = "\n잘못된 입력입니다";
+    public const string IMPOSSIBLE_SELL = "\n판매할 수 없습니다.";
+}
---
+++ CSharpConsole_TextRPGB13/Util/CustomConsole.cs
@@ -0,0 +1,144 @@
+namespace CSharpConsole_TextRPGB13.Util;
+
+public static class CustomConsole
+{
+    public const ConsoleColor PRIMARY_COLOR = ConsoleColor.White;
+    public const ConsoleColor TITLE_COLOR = ConsoleColor.Red;
+    public const ConsoleColor ERROR_COLOR = ConsoleColor.DarkRed;
+    public const ConsoleColor NUMBER_COLOR = ConsoleColor.DarkMagenta;
+    public const ConsoleColor SIGN_COLOR = ConsoleColor.DarkYellow;
+    public const ConsoleColor MARK_COLOR = ConsoleColor.DarkGray;
+    public const ConsoleColor ALERT_COLOR = ConsoleColor.DarkBlue;
+
+    public static void Write(int content)
+    {
+        Console.ForegroundColor = NUMBER_COLOR;
+        Console.Write(content);
+        Console.ForegroundColor = PRIMARY_COLOR;
+    }
+
+    public static void Write(string content)
+    {
+        foreach (var c in content)
+        {
+            if (c == ' ')
+            {
+                Console.Write(' ');
+                continue;
+            }
+            Console.ForegroundColor = c switch
+            {
+                >= '0' and <= '9' => NUMBER_COLOR,
+                ':' or '.' => MARK_COLOR,
+                '>' or '!' or '|' or '+' or '-' => SIGN_COLOR,
+                _ => PRIMARY_COLOR,
+            };
+            Console.Write(c);
+            Console.ForegroundColor = PRIMARY_COLOR;
+        }
+    }
+
+    public static void WriteWithColor(string content, ConsoleColor consoleColor)
+    {
+        Console.ForegroundColor = consoleColor;
+        Console.Write(content);
+        Console.ForegroundColor = PRIMARY_COLOR;
+    }
+
+    public static void WriteLine()
+    {
+        Console.WriteLine();
+    }
+
+    public static void WriteLine(int content)
+    {
+        Console.ForegroundColor = NUMBER_COLOR;
+        Console.WriteLine(content);
+        Console.ForegroundColor = PRIMARY_COLOR;
+    }
+
+    public static void WriteLine(string content)
+    {
+        foreach (var c in content)
+        {
+            if (c == ' ')
+            {
+                Console.Write(' ');
+                continue;
+            }
+            Console.ForegroundColor = c switch
+            {
+                >= '0' and <= '9' => NUMBER_COLOR,
+                ':' or '.' => MARK_COLOR,
+                '>' or '!' or '|' or '+' or '-' => SIGN_COLOR,
+                _ => PRIMARY_COLOR,
+            };
+            Console.Write(c);
+            Console.ForegroundColor = PRIMARY_COLOR;
+        }
+        Console.WriteLine();
+    }
+
+    public static void WriteLineWithColor(string content, ConsoleColor consoleColor)
+    {
+        Console.ForegroundColor = consoleColor;
+        Console.WriteLine(content);
+        Console.ForegroundColor = PRIMARY_COLOR;
+    }
+
+    public static void ConsoleClearByLine(int lineFromBottom)
+    {
+        Console.SetCursorPosition(0, Console.CursorTop - lineFromBottom);
+        for (var i = 0; i < Console.WindowWidth * 2; i++)
+        {
+            Console.Write(' ');
+        }
+        Console.SetCursorPosition(0, Console.CursorTop - lineFromBottom);
+    }
+
+    private static int GetPrintableLength(string str)
+    {
+        var length = 0;
+        foreach (var c in str)
+        {
+            if (char.GetUnicodeCategory(c) == System.Globalization.UnicodeCategory.OtherLetter)
+            {
+                length += 2; // 한글과 같은 넓은 문자에 대해 길이를 2로 취급
+            }
+            else
+            {
+                length += 1; // 나머지 문자에 대해 길이를 1로 취급
+            }
+        }
+
+        return length;
+    }
+
+    public static string PadRightForMixedText(string str, int totalLength)
+    {
+        var currentLength = GetPrintableLength(str);
+        var padding = totalLength - currentLength;
+        return str.PadRight(str.Length + padding);
+    }
+    
+    internal static void ShowTitle(string title)
+    {
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine(title);
+        Console.ResetColor();
+    }
+
+    public static int PromptMenuChoice(int min, int max)
+    {
+        while (true)
+        {
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            Console.WriteLine(">>");
+            if (int.TryParse(Console.ReadLine(), out var choice) && choice >= min && choice <= max)
+            {
+                return choice;
+            }
+            Console.WriteLine("잘못된 입력입니다. 다시 시도해주세요.");
+        }
+    }
+}
